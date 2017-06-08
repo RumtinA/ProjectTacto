fb8a95271274018a9cb6d8253ee88708357b8b06
Tier56 <orlandolrivera@yahoo.com>
2017-06-08 14:23:05 -0400

    Updates

---
+++ Tic tac toe/Assets/Grid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    private int[,] SpaceNumber; //
+
+	// Use this for initialization
+	void Start () {
+        SpaceNumber = new int[3, 3];
+        SpaceNumber [2,2] = 9;
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
