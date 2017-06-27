ae0097bed521d7905c403f6b62e0fd60c60975db
Tier56 <orlandolrivera@yahoo.com>
2017-06-27 10:47:12 -0400

    Feedback Working

---
+++ Tic tac toe/Assets/CameraAspectAdjust.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAspectAdjust : MonoBehaviour {
+
+    public float orthographicSize = 5;
+    public float aspect = 1.33333f;
+
+	// Use this for initialization
+	void Start () {
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(
+            -orthographicSize * aspect, orthographicSize * aspect,
+            -orthographicSize, orthographicSize,
+            Camera.main.nearClipPlane, Camera.main.farClipPlane);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Tic tac toe/Assets/Notification Text/Text Control.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
